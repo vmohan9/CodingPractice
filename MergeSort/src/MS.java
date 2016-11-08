b56c95244dcd8cb083c292ca010c072d423b5e2f
vmohan9 <vahinimohan03@gmail.com>
2016-11-07 23:53:54 -0700

    Merge Sort

---
+++ MergeSort/src/MS.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+
+public class MS {
+	
+	void merge(int arr[],int l,int m,int r){
+		int n1 = m-l+1;
+		int n2 = r-m;
+		int L[] = new int[n1];
+		int M[] = new int[n2];
+		for(int i=0;i<n1;++i)
+			L[i]= arr[l+i];
+		for(int j=0;j<n2;++j){
+			System.out.println(j+" "+m);
+			M[j] = arr[m+1+j];
+			
+		}
+		int i = 0,j=0;
+		int k = l;
+		while(i<n1 && j<n2){
+			if(L[i]<=M[j])
+			{
+				arr[k]=L[i];
+				i++;
+			}
+			else{
+				arr[k]=M[j];
+				j++;
+			}
+			k++;
+		}
+		while(i<n1){
+			arr[k]=L[i];
+			i++;k++;
+		}
+		while(j<n2){
+			arr[k]=M[j];
+			j++;k++;
+		}
+	}
+	
+	void sort(int arr[],int l,int r){
+		if(l<r)
+		{
+			int m = (l+r)/2;
+			sort(arr,l,m);
+			sort(arr,m+1,r);
+			merge(arr,l,m,r);
+			
+		}
+	}
+	
+	public static void main(String[] args){
+		int arr[] = {12, 11, 13, 5, 6, 7};
+		MS ob = new MS();
+		ob.sort(arr, 0 , arr.length-1);
+		System.out.println("sorted array:"+Arrays.toString(arr));
+	}
+}
