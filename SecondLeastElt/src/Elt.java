64026549f264b79e18d8d887c717a14ac32502e6
vmohan9 <vahinimohan03@gmail.com>
2017-02-04 23:54:00 -0700

    Second Least Element
    Second Least Element

---
+++ SecondLeastElt/src/Elt.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Elt {
+
+	public static void main(String[] args){
+		HashMap<Integer,Integer> hm = new HashMap();
+		hm.put(10, 6);
+		hm.put(5, 1);
+		hm.put(9, 4);
+		hm.put(20, 3);
+		hm.put(30, 8);
+	}
+}
